c06ae0ee7f4905b09fe745d387bf038d898c3d6f
Nawaz Haque <nawazhaque@MacBook-Pro-4.local>
2022-04-29 13:31:26 -0400

    first commit, need last problem

---
+++ FridayCC429.js
@@ -0,0 +1,15 @@
+//1
+
+const arrowMyFunction = (num1, num2) => {
+    return num1 + num2;
+}
+console.log(arrowMyFunction(12,12));
+
+//2
+
+const arrowAdd100 = money => [
+    money +100
+]
+console.log(arrowAdd100(1));
+
+//3
